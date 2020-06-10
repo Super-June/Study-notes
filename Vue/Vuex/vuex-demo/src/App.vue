8812a0bdf538c3ec01c45148c81a243bf7e38461
Super-June <guigen_l@163.com>
2020-06-10 17:05:43 +0800

    Vuex

--- Vue/Vuex/vuex-demo/src/App.vue
+++ Vue/Vuex/vuex-demo/src/App.vue
@@ -1,22 +1,22 @@
 <template>
-	<div>
-		<my-addition></my-addition>
-		<p>----------------------------------</p>
-		<my-subtraction></my-subtraction>
-	</div>
+    <div>
+        <my-addition></my-addition>
+        <p>----------------------------------</p>
+        <my-subtraction></my-subtraction>
+    </div>
 </template>
 
 <script>
-	import Addition from './components/Addition.vue';
-	import Subtraction from './components/Subtraction.vue';
+import Addition from "./components/Addition.vue"
+import Subtraction from "./components/Subtraction.vue"
 
-	export default {
-		data() {
-			return {};
-		},
-		components: {
-			'my-addition': Addition,
-			'my-subtraction': Subtraction
-		}
-	};
+export default {
+    data() {
+        return {}
+    },
+    components: {
+        "my-addition": Addition,
+        "my-subtraction": Subtraction
+    }
+}
 </script>
